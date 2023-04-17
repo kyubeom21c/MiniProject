c18e418e8718a0b22144cb9fa1e863d4925e0230
woong <slahsrh2@naver.com>
2023-04-17 15:29:09 +0900

    웅이의 푸시~

--- AMS_Lib/AMS_Lib.cpp
+++ AMS_Lib/AMS_Lib.cpp
@@ -3,6 +3,10 @@
 
 #include "pch.h"
 #include "framework.h"
+#include <cmath>
+#include <iostream>
+
+using namespace std;
 
 // TODO: 라이브러리 함수의 예제입니다.
 
@@ -10,7 +14,34 @@ class AMSDynamics
 {
 public:
 
+	void NextPos(double _pos_x, double _pos_y, double _angle)
+	{
+		double pos_x = _pos_x;
+		double pos_y = _pos_y;
+		double angle = _angle;
+
+		double speed = 340.0 * 5.0; // 마하 5
+		double timestep = 0.1;
+
+		double next_pos_x;
+		double next_pos_y;
+
+		next_pos_x = pos_x + (speed * cos(angle));
+		next_pos_y = pos_y + (speed * sin(angle));
+		cout << "curr_x : " << pos_x << "curr_y : " << pos_y << endl;
+		cout << "next_x : " << next_pos_x << "next_y : " << next_pos_y << endl;
+	}
+
 };
 void fnAMSLib()
 {
+
 }
+
+int main()
+{
+	cout << "hello world" << endl;
+//	AMSDynamics ams = AMSDynamics();
+	//ams.NextPos(10.1, 10.1, 3.14 / 6);
+	
+};
