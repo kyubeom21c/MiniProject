17024ac82303acfc58e83c55ed3445fbb00119c0
JuYeong98 <twinjuy@gmail.com>
2023-04-17 17:27:56 +0900

    공중위협의 발사각도 구하는 코드 작성

--- AMS_Lib/AMS_Lib.cpp
+++ AMS_Lib/AMS_Lib.cpp
@@ -12,32 +12,56 @@ using namespace std;
 class AMSDynamics
 {
 public:
+double samplingRate;
 
+    // 이동 및 좌표 갱신 함수
     void NextPos(double _pos_x, double _pos_y, double _angle)
     {
-        double pos_x = _pos_x;
-        double pos_y = _pos_y;
-        double angle = _angle;
+        curr_pos_x = _pos_x;
+        curr_pos_y = _pos_y;
+        AMSangle = _angle;
 
-        double speed = 340.0 * 5.0; // 마하 5 를 초당 속도(미터)로 변환
+        
         double timestep = 0.1;
-
-        double next_pos_x;
-        double next_pos_y;
-
-        next_pos_x = pos_x + (speed * cos(angle) * timestep);
-        next_pos_y = pos_y + (speed * sin(angle) * timestep);
-        cout << "curr_x : " << pos_x << "curr_y : " << pos_y << endl;
+                
+        next_pos_x = curr_pos_x + (AMSspeed * cos(AMSangle) * timestep);
+        next_pos_y = curr_pos_y + (AMSspeed * sin(AMSangle) * timestep);
+        cout << "curr_x : " << curr_pos_x << "curr_y : " << curr_pos_y << endl;
         cout << "next_x : " << next_pos_x << "next_y : " << next_pos_y << endl;
     }
 
-    void FireAngle(double _ATSPox_y, double _ATSPos_y, double ATSspeed)
+    void FireAngle(double _ATSPos_Initial_x, double _ATSPos_Initial_y, double _ATSPos_Target_x, double _ATSPos_Target_y, double ATSspeed)
     {
+        // 시나리오로부터 공중위협의 초기위치, 목적위치로부터 공중위협의 비행 각도를 구한다.
+        // ATSang = (y_target - y_init) / (x_target - x_init)
 
+        // 
+        double ATS_dX =  (_ATSPos_Target_x - _ATSPos_Initial_x);
+        double ATS_dY= (_ATSPos_Target_y - _ATSPos_Initial_y);
+
+        double ATS_Rad = atan2(ATS_dY, ATS_dX);
+        cout << ATS_Rad;
+       
     }
+private:
+    double curr_pos_x; //대공유도탄의 현재 위치 중 x좌표
+    double curr_pos_y; //대공유도탄의 현재 위치 중 y좌표
+
+    double next_pos_x; //대공유도탄의 다음 위치 중 x좌표
+    double next_pos_y; //대공유도탄의 다음 위치 중 y좌표
+   
+
+
+    double AMSangle; // 대공유도탄의 발사 각도
+    double AMSspeed = 340 * 5; // 대공유도탄 속력 (마하5 의 초당 속력 m)
+
+    double ATSPos_x; //공중위협의 위치 중 x 좌표
+    double ATSPos_y; //공중위협의 위치 중 y 좌표
+
 };
 void fnAMSLib()
 {
+
 
 }
 
